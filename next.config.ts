48b5ec4b81c43b215e046b1e6b077153755aa88a
Olexii Zhevakin <o.zhevakin.frontenddev@gmail.com>
2026-07-11 18:35:33 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/uk",
+        permanent: true, // Вказує, що це постійний редирект (301), що найкраще для SEO
+      },
+    ];
+  },
 };
 
 export default nextConfig;
