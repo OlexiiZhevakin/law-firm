3229766947b21066da853404e534d9c396f47231
Olexii Zhevakin <o.zhevakin.frontenddev@gmail.com>
2026-07-13 13:48:44 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1331',
-        pathname: '/uploads/**',
+        hostname: '31.131.18.174', // Ваш IP
+        port: '1331',              // Ваш порт
+        pathname: '/uploads/**',   // Шлях, де Strapi зберігає файли
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
