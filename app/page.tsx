94be8c1d0541f92b0517b6c05334579a238c6cf8
Olexii Zhevakin <o.zhevakin.frontenddev@gmail.com>
2026-07-13 13:24:53 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-// export default function RootPage() {
-//   // Цей файл спрацює, коли користувач перейде на harlib.com.ua/
-//   // Він миттєво перенаправить його в папку [locale] з параметром 'uk'
-//   redirect('/uk');
-// }
+export default function RootPage() {
+  // Цей файл спрацює, коли користувач перейде на harlib.com.ua/
+  // Він миттєво перенаправить його в папку [locale] з параметром 'uk'
+  redirect('/uk');
+}
