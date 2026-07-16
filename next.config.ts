80ddd59bdac3e68d449b4449715cf84f98f21680
Olexii Zhevakin <o.zhevakin.frontenddev@gmail.com>
2026-07-17 00:37:13 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,71 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: '31.131.18.174', // Ваш IP
+//         port: '1331',              // Ваш порт
+//         pathname: '/uploads/**',   // Шлях, де Strapi зберігає файли
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https: http://31.131.18.174:1331;
+  font-src 'self' data:;
+  connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com http://31.131.18.174:1331;
+  require-trusted-types-for 'script';
+`.replace(/\n/g, '').replace(/\s+/g, ' ').trim();
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '31.131.18.174', // Ваш IP
-        port: '1331',              // Ваш порт
-        pathname: '/uploads/**',   // Шлях, де Strapi зберігає файли
+        hostname: '31.131.18.174',
+        port: '1331',
+        pathname: '/uploads/**',
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ]
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
