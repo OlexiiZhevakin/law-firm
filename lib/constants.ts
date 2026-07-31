1d244cd499f797115dd03006b8821d1730d6eb7b
Olexii Zhevakin <o.zhevakin.frontenddev@gmail.com>
2026-07-31 10:49:57 +0300

    Додано ключ до гугл serch console

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,5 +7,5 @@ export const BASE_URL = 'https://harlib.com.ua';
 export const CONTACT_EMAIL = 'info@harlib.com.ua';
 export const CONTACT_PHONE = '+380933490202';
 
-export const GOOGLE_SITE_VERIFICATION = 'ajEeltqhnCaDdkrysooitKpCvmRwhLTtdBcWm9icdyA';
+export const GOOGLE_SITE_VERIFICATION = 'jMcFYWApl0mvFZ7ATbgMHZJx6jR7-Ql6yQ11GmZBzsE';
 export const GA_MEASUREMENT_ID = 'G-PLXGLCQLSF';
